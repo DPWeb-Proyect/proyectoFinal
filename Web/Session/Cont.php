b759bd5fddaed49003a70560ecf1b33024d52b2f
Darius-js <64049613+Darius-js@users.noreply.github.com>
2020-06-02 21:19:04 -0600

    Estructura del sitio web
    Estructura del sitio web se divide en dos secciones una pagina html para vista y las php que permiten hacer la reserva

---
+++ Web/Session/Cont.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/Master.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+</head>
+<body>
+    <div id="Encabezado">
+        <table class="head">
+            <tr>
+                <td class="igual">
+
+                </td>
+                <td class="igual">
+
+                </td>
+                <td class="inicio">
+                <a id="Secion" href="#">Cerrar Sesi&oacute;n</a>
+                </td>
+            </tr>    
+        </table>
+    </div>
+    <nav><ul>
+        <li><a href="./Index.php">Inicio</a></li>
+        <li><a href="./Us.php">Quiénes somos</a></li>
+        <li><a href="./habit/Hab.php">Habitaciones</a>
+            <ul><li><a href="./habit/Eco.php">Economica</a></li>
+            <li><a href="./habit/Pre.php">Premium</a></li>
+            <li><a href="./habit/Pen.php">Penhouse</a></li>
+            </ul>            </li>
+        
+        <li><a href="./Cont.php">Contacto</a></li>
+    </ul></nav>
+</body>
+</html>
